61e069d23746a104eee9cca0b288c42f72caf52f
Alok Kumar <alokrkmv12@gmail.com>
2022-10-20 17:44:13 -0400

    Added the test cases and test design doc

--- src/tests/test_3/test3.py
+++ src/tests/test_3/test3.py
@@ -3,14 +3,29 @@ import os
 from create_bazar import get_hop_count, create_bazar
 from peers import Peer
 
+"""
+Test case 1
+Description: There are 5 peers in the network. 1 buyer and 4 sellers. 
+Buyer requests for salt and the sellers are of salt. The sellers initially has 2 salt items to sell. 
+The buyer starts a lookup, and matches with single/multiple sellers. But randomly selects any seller.
+That seller sells 1 salt item to the buyer and its total count decreases.
+
+Result: Everytime buyer gets replies from multiple sellers, it selects 1 seller randomly and the item count of only that seller decreases.
+"""
+
+
 
 ids = {
     "buyer0": "buyer",
-    "seller1": "seller"
+    "seller1": "seller",
+    "seller2": "seller",
+    "seller3": "seller",
+    "seller4": "seller",
+
 }
 
 items = ["fish","salt","boar"]
-items_count = 5
+items_count = 2
 hostname = "localhost"
 base_path = os.getcwd()
 peers = []
@@ -33,6 +48,8 @@ for peer in peers:
 
     if peer.role == "seller":
         peer.item = ["fish","salt","boar"][1]
+        peer.max_items = 2
+
     print(peer.role, peer.item)
 
 for peer in peers:
